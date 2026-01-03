5836303a087e7b53eb0e7a50b2179b731d08c122
mohamedredachakir <chakirm082@gmail.com>
2026-01-03 15:25:38 +0100

    create logic for reouter whit class router and 3 methodes get post and dispatch and fix index

--- App/Core/router.php
+++ App/Core/router.php
@@ -3,5 +3,28 @@
 namespace App\Core;
 
 class Router {
-    
+    private array $routes = [];
+    public function get($url,$controller){
+        $this->routes['GET'][$url] = $controller;
+    }
+    public function post($url, $controller){
+        $this->routes['POST'][$url] = $controller;
+    }
+    public function dispatch(){
+        $url = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+        $requestmethode = $_SERVER['REQUEST_METHOD'];
+        if(isset($this->routes[$requestmethode][$url])){
+            [$controllerName , $methodeName] = explode('@', $this->routes[$requestmethode][$url]);
+            $controllerClass = "App\\Controllers\\$controllerName";
+            $controller = new $controllerClass();
+            $controller->$methodeName();
+            return;
+        }else{
+            $controllerClass = "App\\controllers\\404Controller";
+            $controller = new $controllerClass();
+            $controller->index();
+            return;
+        };
+
+    }
 }
--- Public/index.php
+++ Public/index.php
@@ -1,10 +1,11 @@
 <?php
 
-require_once __DIR__ .'/../bootstrap/autoload.php';
+require_once __DIR__ . '/../bootstrap/autoload.php';
+require __DIR__ . '/../App/Core/db.php';
 
-use App\Core\router;
-
+use App\Core\Router;
 
 session_start();
 
 $router = new Router();
+$router->get('/','HomeController@index');
