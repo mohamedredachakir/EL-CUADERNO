723e335faacdf8fc4a3ca93748ecaa4cff813bc9
mohamedredachakir <chakirm082@gmail.com>
2026-01-04 18:43:44 +0100

    create methodes for class user in model one for findemail and secound is create and create logic for 3 methodes in authcontroller one for register and secound for login and last for logout

---
+++ App/Core/db.php
@@ -0,0 +1,26 @@
+<?php
+
+class Database {
+    private $host = "localhost";
+    private $db_name = "el_cuaderno";
+    private $user_name = "root";
+    private $password = "197170";
+    private $conn = null;
+    
+    public function getconnection(){
+        if($this->conn === null){
+            try{
+                $this->conn = new PDO(
+                    "mysql:host=".$this->host.";dbname=".$this->db_name.";charset=utf8mb4",
+                    $this->user_name,$this->password
+                );
+            }catch(PDOException $e){
+                echo "connection failed" . $e->getMessage();
+            }
+        }
+        return $this->conn;
+    }
+}
+
+$database = new Database();
+$conn = $database->getconnection();
